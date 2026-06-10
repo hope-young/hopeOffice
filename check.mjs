416b7c66a82e9557dc626399866ae7fe7990ce87
hope_young <hope1678481280@gmail.com>
2026-06-10 10:47:07 +0800

    \drop check.mjs"

--- check.mjs
+++
@@ -1,4 +0,0 @@
-import { SKILL_REGISTRY, listSkillsForHost } from './src/core/skills/index.ts'
-const names = listSkillsForHost('excel').map(s => s.name).sort()
-console.log('count:', names.length)
-console.log('names:', names)
