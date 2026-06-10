49be48b74883c289daa01aa50f37ad462c1eb8a8
hope_young <hope1678481280@gmail.com>
2026-06-10 15:06:55 +0800

    \drop debug-md.mjs"

--- debug-md.mjs
+++
@@ -1,4 +0,0 @@
-import { parseBlocks, parseInline, splitThinking } from './src/taskpane/components/Markdown.tsx'
-
-const md = '- top\n  - mid\n    - leaf'
-console.log(JSON.stringify(parseBlocks(md), null, 2))
